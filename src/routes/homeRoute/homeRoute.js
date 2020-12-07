9eaf4f77c03a19180ac420a51d03e6e78b21b596
Mark <SwiftTactics@gmail.com>
2020-12-07 15:52:49 -0500

    fixed home typos

--- src/routes/homeRoute/homeRoute.js
+++ src/routes/homeRoute/homeRoute.js
@@ -6,12 +6,12 @@ const HomeRoute = () => {
   return (
     <div className='homeDiv'>
       <h2>Welcome to theSwattr!</h2>
-      <p>theSwattr is a an all in one bug tracking tool for development teams, as well as a ticket system
+      <p>theSwattr is a an all-in-one bug tracking tool for development teams, as well as a ticket system
         for users to submit bugs to the development team.</p>
         <p>
-          This application allows users to contirbute information to the development team so that the user experience can improve,
-           also this application allows developers to smoothly track bugs that are sorted by severity, status, and how long it has existed.
-           Developers in additon can respond to the users through their bugs comments, as well as update the status, severity or which app the bug belongs to.           
+          This application allows users to contribute information to the development team so that the user experience can improve.
+           This application also allows developers to smoothly track bugs that are sorted by severity, and status.
+           Developers in additon can respond to the users through a comments thread in each bug, as well as update the status, severity or which app the bug belongs to.           
         </p>
         <Link to='/login'>
           Lets get started!
